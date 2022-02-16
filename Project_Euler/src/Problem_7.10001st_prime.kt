17472c335b65e8dbf4a4adb46d93922e0ef4ad17
Юрий Обласов <ryanc8109@gmail.com>
2022-02-16 09:56:03 +0300

    Update Problem_7.10001st_prime.kt

--- Project_Euler/src/Problem_7.10001st_prime.kt
+++ Project_Euler/src/Problem_7.10001st_prime.kt
@@ -1,21 +1,21 @@
-import kotlin.math.sqrt
-
 /*
 * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
 * we can see that the 6th prime is 13.
 * What is the 10 001st prime number?
 * */
 
+import kotlin.math.sqrt
+
 fun main() {
     var sequenceNumber = 2
     var primeNumber = 2
-    while (sequenceNumber != 10001){
+    while (sequenceNumber != 10001) {
         primeNumber++
         var t = 2
         val rootOfPrimeNumber = sqrt(primeNumber.toFloat())
-        while (primeNumber%t!=0 && t<=rootOfPrimeNumber){
+        while (primeNumber % t != 0 && t <= rootOfPrimeNumber) {
             t++
-            if (t>rootOfPrimeNumber){
+            if (t > rootOfPrimeNumber) {
                 sequenceNumber++
             }
         }

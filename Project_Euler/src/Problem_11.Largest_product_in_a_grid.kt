24806b3ce4cf491c1d61d73fcb3947c93ee06681
Юрий Обласов <ryanc8109@gmail.com>
2022-02-20 17:49:32 +0300

    11_problem

--- Project_Euler/src/Problem_11.Largest_product_in_a_grid.kt
+++ Project_Euler/src/Problem_11.Largest_product_in_a_grid.kt
@@ -25,7 +25,8 @@
 * direction (up, down, left, right, or diagonally) in the 20×20 grid?
 * */
 
-val stringOfNumbers = """
+fun main() {
+    val stringOfNumbers = """
         08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
         49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
         81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
@@ -46,8 +47,45 @@ val stringOfNumbers = """
         20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
         20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
         01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
-""".trimIndent()
+"""
+        .trimIndent()
+        .split("\n")
+        .toTypedArray()
 
-fun main() {
-    print(stringOfNumbers)
+    val size = 20
+    val lengthOfProduct = 4
+    val array = Array(size) { Array(size) { 0 } } // можно ли заполнить сразу???
+    for (i in stringOfNumbers.indices) {
+        var counter = 0
+        for (j in stringOfNumbers[i].split(" ")) {
+            array[i][counter++] = j.toInt()
+        }
+    }
+
+    var greatestProduct = 0
+    for (row in array.indices) {
+        for (cell in array[row].indices) {
+            // можно ли обойтись без говнокодинга? DRY!!!
+            val line = if (cell < size - lengthOfProduct)
+                array[row][cell] * array[row][cell + 1] * array[row][cell + 2] * array[row][cell + 3]
+            else 0
+            greatestProduct = if (greatestProduct < line) line else greatestProduct
+
+            val column = if (row < size - lengthOfProduct)
+                array[row][cell] * array[row + 1][cell] * array[row + 2][cell] * array[row + 3][cell]
+            else 0
+            greatestProduct = if (greatestProduct < column) column else greatestProduct
+
+            val diagonalUpLeft = if (row < size - lengthOfProduct && cell < size - lengthOfProduct)
+                array[row][cell] * array[row + 1][cell + 1] * array[row + 2][cell + 2] * array[row + 3][cell + 3]
+            else 0
+            greatestProduct = if (greatestProduct < diagonalUpLeft) diagonalUpLeft else greatestProduct
+
+            val diagonalUpRight = if (row < size - lengthOfProduct && cell >= lengthOfProduct - 1)
+                array[row][cell] * array[row + 1][cell - 1] * array[row + 2][cell - 2] * array[row + 3][cell - 3]
+            else 0
+            greatestProduct = if (greatestProduct < diagonalUpRight) diagonalUpRight else greatestProduct
+        }
+    }
+    print(greatestProduct)
 }

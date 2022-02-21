5d72f264e0816db1466593406864e267fdeb7460
Юрий Обласов <ryanc8109@gmail.com>
2022-02-21 09:20:33 +0300

    Something

--- Lessons/src/Условные операторы.kt
+++ Lessons/src/Условные операторы.kt
@@ -1,19 +1,34 @@
 fun main(args: Array<String>) {
-    var word: String? = null
-    var isHasCar = true
+    println("Введите два числа x, y:")
+    val (x, y) = readln().split(" ").map(String::toInt)
 
-    //if(word == null)
-    //    print("Переменная word равна значению null")
-    // если внутри иф одна строчка, фигурные скобки можно не писать {}
+    if (x > y) {
+        println("x > y, $x > $y")
+    } else if (x < y) {
+        println("x < y, $x < $y")
+    } else {
+        println("x = y, $x = $y")
+    }
 
-    if(word == null){
+    val xIsGreaterThanY = if (x > y) {
+        println("x > y, $x > $y")
+        1
+    } else {
+        println("x <= y, $x <= $y")
+        0
+    }
+
+
+    val word: String? = null
+    var isHasCar: Boolean
+    if (word == null) {
         isHasCar = false
-        if(!isHasCar || word == null){ // || - or, && - and
+        if (!isHasCar || word == null) { // || - or, && - and
             println("Что-то сработало")
         }
         println("Чsergsg")
         println("esgeg")
-    } else  if(word == "Hello") {
+    } else if (word == "Hello") {
         println("Переменная word равна $word")
         println("else if")
     } else {
@@ -21,25 +36,20 @@ fun main(args: Array<String>) {
         println("word $word")
     }
 
-    val number = 5
-    when(number){
+    val number = if (x > y) readln().toInt() else null
+    when (number) {
         null -> println("Переменная number равна null")
         10 -> println("Переменная равна 10")
         5 -> println("Переменная равна 5")
+        in 1..8 -> println("В диапазоне от 1 до 8")
+        in 9..100 -> println("In range 9 to 100")
         else -> println("Переменная какая-то непонятная")
     }
 
-//    var res = 0
-//    if(number > 30)
-//        res = 1
-//    else
-//        res = 0
-// ниже представлена сокращенная запись
-
-    var res = if(number > 30) 1 else 0
+    var res = if (number != null && number > 30) 1 else 0
 
 
-    var words = when(number){
+    var words = when (number) {
         null -> "Ничего"
         else -> number
     }
